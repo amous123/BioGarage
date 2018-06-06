c10973e7e47cdf39286276e89c0cf25ea419df3a
amous123 <31967955+amous123@users.noreply.github.com>
2018-06-06 09:18:08 -0400

    modified front page animation and modified navbar css on hover

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+/* --- function to transition frontpage main background smoothly --- */
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" pressed", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " pressed";
+}
+
 /* ---- expand and retract sideBar content and overlay ---- */
 function on() {
   var over = document.getElementById("overlay");
