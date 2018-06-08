c19d2a26ec887a6c7d5c3b3b51669808351f0230
amous123 <31967955+amous123@users.noreply.github.com>
2018-06-08 15:23:08 -0400

    moved image seperator to about page, added partivle effects as filler to main page, fixed some bugs, changed frontpage zoom in animation to a flash, centered footer in mobile

---
+++ standard.js
@@ -0,0 +1,481 @@
+/* --- sets navbar transparent at top of page --- */
+$(window).scroll(function () {
+if ($(document).scrollTop() >= 100) {
+$("ul.topnav").removeClass('transparent');
+} else {
+$("ul.topnav").addClass('transparent');
+}
+});
+
+
+/* --- function to transition frontpage main background smoothly --- */
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].id = "transparentHomeButtons";
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].id = "";
+}
+
+/* ---- expand and retract sideBar content and overlay ---- */
+function on() {
+  document.getElementById("overlay").style.width="100%";
+  document.getElementById("menuoverlay").style.width="340px";
+  document.getElementById("blurContain").className="blurContainerSet";
+}
+
+function off() {
+    document.getElementById("overlay").style.width="0%";
+    document.getElementById("menuoverlay").style.width="0px";
+    document.getElementById("blurContain").className="blurContainerUnset";
+}
+
+/* ---- expand and retract about section content ---- */
+function expand1(){
+  var p1 = document.getElementById("about1");
+  p1.className = "show";
+    document.getElementById("expandB1").style.display="none";
+    document.getElementById("retractB1").style.display="inline-block";
+}
+function expand2(){
+  var p2 = document.getElementById("about2");
+  p2.className = "show";
+    document.getElementById("expandB2").style.display="none";
+    document.getElementById("retractB2").style.display="inline-block";
+}
+function expand3(){
+  var p3 = document.getElementById("about3");
+  p3.className = "show";
+    document.getElementById("expandB3").style.display="none";
+    document.getElementById("retractB3").style.display="inline-block";
+}
+
+function retract1(){
+  var p1 = document.getElementById("about1");
+  p1.className = "hide";
+  document.getElementById("expandB1").style.display="inline-block";
+  document.getElementById("retractB1").style.display="none";
+}
+function retract2(){
+  var p2 = document.getElementById("about2");
+  p2.className = "hide";
+  document.getElementById("expandB2").style.display="inline-block";
+  document.getElementById("retractB2").style.display="none";
+}
+function retract3(){
+  var p3 = document.getElementById("about3");
+  p3.className = "hide";
+  document.getElementById("expandB3").style.display="inline-block";
+  document.getElementById("retractB3").style.display="none";
+}
+
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 180, density: { enable: true, value_area: 800 } },
+    color: { value: "#4a4a4a" },
+    shape: {
+      type: "circle",
+      stroke: { width: 0, color: "#000000" },
+      polygon: { nb_sides: 5 },
+      image: { src: "img/github.svg", width: 100, height: 100 }
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+    },
+    size: {
+      value: 3.945738208161363,
+      random: true,
+      anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
+    },
+    line_linked: {
+      enable: false,
+      distance: 94.69771699587272,
+      color: "#f1f1f1",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 3.1565905665290903,
+      direction: "top-right",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 1200 }
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: true, mode: "grab" },
+      onclick: { enable: true, mode: "repulse" },
+      resize: true
+    },
+    modes: {
+      grab: { distance: 200, line_linked: { opacity: 1 } },
+      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+      repulse: { distance: 200, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 }
+    }
+  },
+  retina_detect: true
+});
+
+update = function() {
+  stats.begin();
+  stats.end();
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  }
+  requestAnimationFrame(update);
+};
+requestAnimationFrame(update);
+
+
+/*
+// Add smooth scrolling to all links
+$(document).ready(function(){
+    $("#web-view").fadeOut();
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+    // Fade out image when switch is clicked
+    $('#features-switch').click(function() {
+        if($("#features-switch").is(':checked')){
+            $("#mobile-view").fadeOut();
+            $("#web-view").fadeIn();
+        } else {
+            $("#mobile-view").fadeIn();
+            $("#web-view").fadeOut();
+        }
+    });
+});
+*/
+
+// Predefined code for using Google Maps Static Javascript API
+function initMap() {
+    var uluru = {lat: 45.420956, lng: -75.681306};
+
+    var style2 = [
+        {
+            "featureType": "all",
+            "elementType": "labels",
+            "stylers": [
+                {
+                    "visibility": "on"
+                }
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "saturation": 36
+                },
+                {
+                    "color": "#000000"
+                },
+                {
+                    "lightness": 40
+                }
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels.text.stroke",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "color": "#232323"
+                }
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels.icon",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "administrative",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#000000"
+                },
+                {
+                    "lightness": 20
+                }
+            ]
+        },
+        {
+            "featureType": "administrative",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "color": "#000000"
+                },
+                {
+                    "lightness": 17
+                },
+                {
+                    "weight": 1.2
+                }
+            ]
+        },
+        {
+            "featureType": "administrative.country",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#FFC80A"
+                }
+            ]
+        },
+        {
+            "featureType": "administrative.locality",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#c4c4c4"
+                }
+            ]
+        },
+        {
+            "featureType": "administrative.neighborhood",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#FFC80A"
+                }
+            ]
+        },
+        {
+            "featureType": "landscape",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#000000"
+                },
+                {
+                    "lightness": 20
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#000000"
+                },
+                {
+                    "lightness": 21
+                },
+                {
+                    "visibility": "on"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.business",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "visibility": "on"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#FFC80A"
+                },
+                {
+                    "lightness": "0"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#ffffff"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "labels.text.stroke",
+            "stylers": [
+                {
+                    "color": "#FFC80A"
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#000000"
+                },
+                {
+                    "lightness": 18
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#575757"
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#ffffff"
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "labels.text.stroke",
+            "stylers": [
+                {
+                    "color": "#2c2c2c"
+                }
+            ]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#232323"
+                }
+            ]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#999999"
+                }
+            ]
+        },
+        {
+            "featureType": "transit",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#000000"
+                },
+                {
+                    "lightness": 19
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#000000"
+                },
+                {
+                    "lightness": 17
+                }
+            ]
+        }
+    ];
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 16,
+        center: uluru,
+        styles: style2
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
+
+$(".bg").interactive_bg({
+   strength: 25,
+   scale: 1.05,
+   animationSpeed: "100ms",
+   contain: true,
+   wrapContent: false
+ });
+ $(document).ready(function(){
+    $(".bg").interactive_bg(); // function call
+});
+
+  // change background size on window resize
+  $(window).resize(function() {
+      $(".bg > .ibg-bg").css({
+        width: $(window).outerWidth(),
+        height: $(window).outerHeight()
+      })
+   })
