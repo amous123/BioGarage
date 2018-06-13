8e009c31240b11e06af5fb64136f8f04e030fa52
amous123 <31967955+amous123@users.noreply.github.com>
2018-06-13 16:47:01 -0400

    fixed some bugs on front page and added featured pictures

--- standard.js
+++ standard.js
@@ -33,6 +33,53 @@ function showDivs(n) {
   dots[slideIndex-1].id = "";
 }
 
+/* --- functions to show and hide camera icon --- */
+function fa1(){document.getElementById('fa1').style.opacity = '1';}
+function fa2(){document.getElementById('fa2').style.opacity = '1';}
+function fa3(){document.getElementById('fa3').style.opacity = '1';}
+function fa4(){document.getElementById('fa4').style.opacity = '1';}
+function fa1C(){document.getElementById('fa1').style.opacity = '0';}
+function fa2C(){document.getElementById('fa2').style.opacity = '0';}
+function fa3C(){document.getElementById('fa3').style.opacity = '0';}
+function fa4C(){document.getElementById('fa4').style.opacity = '0';}
+
+/* --- functions to open modal that displays photos --- */
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+  document.getElementById("topnav").style.display = "none";
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+  document.getElementById('topnav').style.display = "unset";
+}
+
+function plusSlides(n) {
+  showSlides(gallerySlideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(gallerySlideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides2");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {gallerySlideIndex = 1}
+  if (n < 1) {gallerySlideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[gallerySlideIndex-1].style.display = "block";
+  dots[gallerySlideIndex-1].className += " active";
+  captionText.innerHTML = dots[gallerySlideIndex-1].alt;
+}
+
 /* ---- expand and retract sideBar content and overlay ---- */
 $(document).ready(function(){
   $('#sitemenu-icon').click(function(){
@@ -41,9 +88,19 @@ $(document).ready(function(){
     $('#blurContain').toggleClass('active');
     $('#menuoverlay').toggleClass('active');
     $('#logoHolder').toggleClass('active');
+    $('ul.topnav').toggleClass('active');
+  })
+  $('#overlay').click(function(){
+    $('#sitemenu-icon').toggleClass('active');
+    $('#overlay').toggleClass('active');
+    $('#blurContain').toggleClass('active');
+    $('#menuoverlay').toggleClass('active');
+    $('#logoHolder').toggleClass('active');
+    $('ul.topnav').toggleClass('active');
   })
 })
 
+/* --- changeable details on the particle.js algorithim  --- */
 particlesJS("particles-js", {
   particles: {
     number: { value: 100, density: { enable: true, value_area: 800 } },
