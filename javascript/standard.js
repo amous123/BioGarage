6a507b3c79ba7ae2fd0b1696e361cc0d745669f1
amous123 <31967955+amous123@users.noreply.github.com>
2018-06-20 09:43:05 -0400

    fixed bug that didnt open sidebar

---
+++ javascript/standard.js
@@ -0,0 +1,159 @@
+/* --- sets navbar transparent at top of page --- */
+$(window).scroll(function() {
+if ($(document).scrollTop() >= 100) {
+$("ul.topnav").removeClass('transparent');
+} else {
+$("ul.topnav").addClass('transparent');
+}
+});
+
+$(window).load(function() {
+  $("body").removeClass("preload");
+});
+
+/* --- function to transition frontpage main background smoothly --- */
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].id = "transparentHomeButtons";
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].id = "";
+}
+
+/* ---- expand and retract sideBar content and overlay ---- */
+$(document).ready(function(){
+  $('#sitemenu-icon').click(function(){
+    $('#sitemenu-icon').toggleClass('active');
+    $('#overlay').toggleClass('active');
+    $('#blurContain').toggleClass('active');
+    $('#menuoverlay').toggleClass('active');
+    $('#logoHolder').toggleClass('active');
+    $('ul.topnav').toggleClass('active');
+  });
+  $('#overlay').click(function(){
+    $('#sitemenu-icon').toggleClass('active');
+    $('#overlay').toggleClass('active');
+    $('#blurContain').toggleClass('active');
+    $('#menuoverlay').toggleClass('active');
+    $('#logoHolder').toggleClass('active');
+    $('ul.topnav').toggleClass('active');
+  })
+});
+
+
+/* --- changeable details on the particle.js algorithim  --- */
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 52, density: { enable: true, value_area: 600 } },
+    color: { value: "#000000" },
+    shape: {
+      type: "circle",
+      stroke: { width: 0, color: "#000000" },
+      polygon: { nb_sides: 10 },
+      image: { src: "images/bioLogo_avatar.png", width: 100, height: 100 }
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+    },
+    size: {
+      value: 5,
+      random: true,
+      anim: { enable: false, speed: 40, size_min: 2, sync: false }
+    },
+    line_linked: {
+      enable: true,
+      distance: 170,
+      color: "#ffffff",
+      opacity: 0.5,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 6,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 1200 }
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: true, mode: "grab" },
+      onclick: { enable: true, mode: "push" },
+      resize: true
+    },
+    modes: {
+      grab: { distance: 200, line_linked: { opacity: 1 } },
+      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+      repulse: { distance: 200, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 }
+    }
+  },
+  retina_detect: true
+});
+
+update = function() {
+  stats.begin();
+  stats.end();
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  }
+  requestAnimationFrame(update);
+};
+requestAnimationFrame(update);
+
+
+
+$(".bg").interactive_bg({
+   strength: 25,
+   scale: 1.05,
+   animationSpeed: "100ms",
+   contain: true,
+   wrapContent: false
+ });
+ $(document).ready(function(){
+    $(".bg").interactive_bg(); // function call
+});
+
+  // change background size on window resize
+  $(window).resize(function() {
+      $(".bg > .ibg-bg").css({
+        width: $(window).outerWidth(),
+        height: $(window).outerHeight()
+      })
+   })
+
+
+/*
+   $(document).ready(function(){
+    $(window).resize();
+});
+$(window).resize(function{
+    // your code
+    var windowWidth=$(window).width();
+    var mainContainerWidth=windowWidth-100; // For example
+    $("#yourMainContainer").css({"width":mainContainerWidth+"px"});
+});
+*/
