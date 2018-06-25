6d103d7ef6e9f82f0fa3f375afee6c6e0e8919d5
amous123 <31967955+amous123@users.noreply.github.com>
2018-06-25 12:32:14 -0400

    working on team page and projects page and updating footer for all pages

--- javascript/projects.js
+++ javascript/projects.js
@@ -26,3 +26,63 @@ $(document).ready(function(){
     $('ul.topnav').toggleClass('active');
   })
 })
+
+$(document).ready(function(){
+  $('.expandable').hover(function(){
+    $('.oneLiner').toggleClass('active');
+    $('.moreContent').toggleClass('active');
+  });
+});
+$(document).ready(function(){
+  $('.expandable').click(function(){
+    $('.oneLiner').toggleClass('active');
+    $('.moreContent').toggleClass('active');
+  });
+});
+
+/* --- functions to show and hide camera icon --- */
+function fa1(){document.getElementById('fa1').style.opacity = '1';}
+function fa2(){document.getElementById('fa2').style.opacity = '1';}
+function fa3(){document.getElementById('fa3').style.opacity = '1';}
+function fa4(){document.getElementById('fa4').style.opacity = '1';}
+function fa1C(){document.getElementById('fa1').style.opacity = '0';}
+function fa2C(){document.getElementById('fa2').style.opacity = '0';}
+function fa3C(){document.getElementById('fa3').style.opacity = '0';}
+function fa4C(){document.getElementById('fa4').style.opacity = '0';}
+
+/* --- functions to open modal that displays photos --- */
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+  document.getElementById("topnav").style.display = "none";
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+  document.getElementById('topnav').style.display = "unset";
+}
+
+function plusSlides(n) {
+  showSlides(gallerySlideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(gallerySlideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides2");
+  var dots = document.getElementsByClassName("demo2");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {gallerySlideIndex = 1}
+  if (n < 1) {gallerySlideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[gallerySlideIndex-1].style.display = "block";
+  dots[gallerySlideIndex-1].className += " active";
+  captionText.innerHTML = dots[gallerySlideIndex-1].alt;
+}
