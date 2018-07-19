bfe3488815de29b2028780d7c8c4b28ff1294abb
amous123 <31967955+amous123@users.noreply.github.com>
2018-07-19 11:10:20 -0400

    modified projects page and working on favicon

--- javascript/projects.js
+++ javascript/projects.js
@@ -1,11 +1,12 @@
 /* ---- changes tabs ---- */
 (function($) {
-var tabs =  $("#tabs li a");
+var tabs =  $("#tabs li");
 var projects =  $(".projectContent");
 tabs.click(function() {
   var content = ($(this).attr('id'));
   tabs.removeClass("active");
   $(this).addClass("active");
+
   content = content.replace('tab', '#project');
   projects.removeClass("active");
   $(content).addClass("active");
